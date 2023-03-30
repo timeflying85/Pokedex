c6b339e5a3c313c5ce8d935e371effadb5b8f2d4
Timé Prégardien <time.pregardien@gmail.com>
2023-03-30 10:32:23 +0200

    updateAll

--- src/app/shared/services/my-service/my-service.service.ts
+++ src/app/shared/services/my-service/my-service.service.ts
@@ -31,7 +31,7 @@ export class MyServiceService {
   }
 
   updateFlight(id: number, flight: FlightInsertForm): Observable<FlightDTO> {
-    return this.http.put<FlightDTO>(this.baseUrl + id, flight)
+    return this.http.put<FlightDTO>(this.baseUrl + id + '/update', flight)
       .pipe(
         catchError((err: HttpErrorResponse) => {
           // handle error and return an observable with error message
